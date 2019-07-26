97524c71cf119b4f5335d04f21b1798ac2ede697
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-07-27 00:29:27 +0700

    delete answers

--- R/run-ipa.R
+++
@@ -1,9 +0,0 @@
-#' Run IPA Random Forest
-#'
-#' Launch shiny application.
-#' @importFrom shiny runApp
-#' @export
-run_ipa <- function() {
-  app_dir <- system.file("ipa", package = "tidyds")
-  shiny::runApp(app_dir, display.mode = "normal")
-}
--- inst/ipa/global.R
+++
@@ -1,8 +0,0 @@
-library(shiny)
-library(here)
-library(recipes)
-library(parsnip)
-
-load(here("data", "un_smp_recipe.rda"))
-load(here("data", "un_smp_lm.rda"))
-load(here("data", "un_smp_rf.rda"))
